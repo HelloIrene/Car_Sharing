ca4e4f67d823333456898872c347db5632fefeb8
Ross.zhang <yuefeiduzi@foxmail.com>
2017-09-01 11:49:02 +0800

    汽车档案编辑frame @2017年8月30日13:30:58 汽车档案编辑frame 大体 @2017年8月30日20:38:48 窗体完成 @2017年8月30日21:22:24 已知BUG：JSpinner换皮肤时有失去光标的空指针异常 @2017年8月30日21:50:00 JSpinner方法设定不能输入非法值，不能越界 @2017年8月31日9:51:53 normal update @2017年8月31日14:09:18 换肤frame @2017年8月31日16:48:23 换肤完成 @2017年8月31日21:03:06 DAO导入完毕 @2017年9月1日11:34:40

--- src/com/company/dao/CommonDAOImpl.java
+++ src/com/company/dao/CommonDAOImpl.java
@@ -12,102 +12,83 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class CommonDAOImpl implements CommonDAO
-{
-	private Connection conn;
+public class CommonDAOImpl implements CommonDAO {
+    private Connection conn;
 
-	@Override
-	public <T> List<T> executeQuery(Class<?> clazz, String query,
-									List<Object> params)
-	{
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		List<T> itemList = new ArrayList<T>();
-		try
-		{
-			conn = DBTool.getInstance().getConnection();
-			// 设置隔离级别
-			conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-			ps = conn.prepareStatement(query);
-			if (params !=null && params.size() > 0)
-			{
-				for (int i = 0; i < params.size(); i++)
-				{
-					ps.setObject(i + 1, params.get(i));
-				}
-			}
-			rs = ps.executeQuery();
-			Field[] fields = clazz.getDeclaredFields();
+    @Override
+    public <T> List<T> executeQuery(Class<?> clazz, String query,
+                                    List<Object> params) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<T> itemList = new ArrayList<T>();
+        try {
+            conn = DBTool.getInstance().getConnection();
+            // 设置隔离级别
+            conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+            ps = conn.prepareStatement(query);
+            if (params != null && params.size() > 0) {
+                for (int i = 0; i < params.size(); i++) {
+                    ps.setObject(i + 1, params.get(i));
+                }
+            }
+            rs = ps.executeQuery();
+            Field[] fields = clazz.getDeclaredFields();
 
-			while (rs.next())
-			{
-				@SuppressWarnings("unchecked")
-				T entity = (T) clazz.newInstance(); //利用反射实例化一个对象
-				for (Field field : fields)
-				{
-					// VERY IMPORTANT!!!
-					field.setAccessible(true);
-					Object rsVal = rs.getObject(field.getName());
+            while (rs.next()) {
+                @SuppressWarnings("unchecked")
+                T entity = (T) clazz.newInstance(); //利用反射实例化一个对象
+                for (Field field : fields) {
+                    // VERY IMPORTANT!!!
+                    field.setAccessible(true);
+                    Object rsVal = rs.getObject(field.getName());
 
-					//##############ORACLE##############
-					Object val = null;
-					if(rsVal.getClass() == BigDecimal.class){
-						if(field.getType() == Double.class){
-							val = ((BigDecimal) rsVal).doubleValue();
-						}else if(field.getType() == Integer.class){
-							val = ((BigDecimal) rsVal).intValue();
-						}else {
-							val = rsVal;
-						}
-					}else {
-						val = rsVal;
-					}
-					//##############ORACLE##############
-					field.set(entity, val);
-				}
-				itemList.add(entity);
-			}
-		}
-		catch (SQLException | InstantiationException | IllegalAccessException
-				| SecurityException e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			DBTool.closeAll(rs, ps, conn);
-		}
-		return itemList;
-	}
+                    //##############ORACLE##############
+                    Object val = null;
+                    if (rsVal.getClass() == BigDecimal.class) {
+                        if (field.getType() == Double.class) {
+                            val = ((BigDecimal) rsVal).doubleValue();
+                        } else if (field.getType() == Integer.class) {
+                            val = ((BigDecimal) rsVal).intValue();
+                        } else {
+                            val = rsVal;
+                        }
+                    } else {
+                        val = rsVal;
+                    }
+                    //##############ORACLE##############
+                    field.set(entity, val);
+                }
+                itemList.add(entity);
+            }
+        } catch (SQLException | InstantiationException | IllegalAccessException
+                | SecurityException e) {
+            e.printStackTrace();
+        } finally {
+            DBTool.closeAll(rs, ps, conn);
+        }
+        return itemList;
+    }
 
-	public int executeUpdate(String query, List<Object> params)
-	{
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		int row = 0;
-		try
-		{
-			conn = DBTool.getInstance().getConnection();
-			conn.setAutoCommit(false);
-			ps = conn.prepareStatement(query);
-			if (params.size() > 0)
-			{
-				for (int i = 0; i < params.size(); i++)
-				{
-					ps.setObject(i + 1, params.get(i));
-				}
-			}
-			row = ps.executeUpdate();
-			conn.commit();
-			conn.setAutoCommit(true);
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-		catch (SecurityException e)
-		{
-			/*try
+    public int executeUpdate(String query, List<Object> params) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int row = 0;
+        try {
+            conn = DBTool.getInstance().getConnection();
+            conn.setAutoCommit(false);
+            ps = conn.prepareStatement(query);
+            if (params.size() > 0) {
+                for (int i = 0; i < params.size(); i++) {
+                    ps.setObject(i + 1, params.get(i));
+                }
+            }
+            row = ps.executeUpdate();
+            conn.commit();
+            conn.setAutoCommit(true);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            /*try
 			{
 				conn.rollback();
 			}
@@ -116,235 +97,182 @@ public class CommonDAOImpl implements CommonDAO
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}*/
-			e.printStackTrace();
-		}
-		finally
-		{
-			DBTool.closeAll(rs, ps, conn);
-		}
-		return row;
-	}
+            e.printStackTrace();
+        } finally {
+            DBTool.closeAll(rs, ps, conn);
+        }
+        return row;
+    }
 
-	public void executeUpdateBatch(String query, Map<Integer, List<Object>> paramsMap)
-	{
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try
-		{
-			conn = DBTool.getInstance().getConnection();
-			conn.setTransactionIsolation(8);
-			conn.setAutoCommit(false);
-			ps = conn.prepareStatement(query);
-			Set<Integer> querySet = paramsMap.keySet();
-			Iterator<Integer> it = querySet.iterator();
-			while (it.hasNext())
-			{
-				Integer idx = it.next();
-				List<Object> params = paramsMap.get(idx);
-				if (params.size() > 0)
-				{
-					for (int i = 0; i < params.size(); i++)
-					{
-						ps.setObject(i + 1, params.get(i));
-					}
-				}
-				ps.addBatch();
-			}
-			ps.executeBatch();
-			conn.commit();
-			conn.setAutoCommit(true);
-		}
-		catch (SQLException | SecurityException e)
-		{
-			try
-			{
-				conn.rollback();
-			}
-			catch (SQLException e1)
-			{
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			e.printStackTrace();
-		}
-		finally
-		{
-			DBTool.closeAll(rs, ps, conn);
-		}
-	}
+    public void executeUpdateBatch(String query, Map<Integer, List<Object>> paramsMap) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = DBTool.getInstance().getConnection();
+            conn.setTransactionIsolation(8);
+            conn.setAutoCommit(false);
+            ps = conn.prepareStatement(query);
+            Set<Integer> querySet = paramsMap.keySet();
+            Iterator<Integer> it = querySet.iterator();
+            while (it.hasNext()) {
+                Integer idx = it.next();
+                List<Object> params = paramsMap.get(idx);
+                if (params.size() > 0) {
+                    for (int i = 0; i < params.size(); i++) {
+                        ps.setObject(i + 1, params.get(i));
+                    }
+                }
+                ps.addBatch();
+            }
+            ps.executeBatch();
+            conn.commit();
+            conn.setAutoCommit(true);
+        } catch (SQLException | SecurityException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+        } finally {
+            DBTool.closeAll(rs, ps, conn);
+        }
+    }
 
-	public void executeUpdateBatch(Map<String, List<Object>> queryMap)
-	{
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try
-		{
-			conn = DBTool.getInstance().getConnection();
-			conn.setTransactionIsolation(8);
-			conn.setAutoCommit(false);
-			Set<String> querySet = queryMap.keySet();
-			Iterator<String> it = querySet.iterator();
-			while (it.hasNext())
-			{
-				String query = it.next();
-				ps = conn.prepareStatement(query);
-				List<Object> params = queryMap.get(query);
-				if (params.size() > 0)
-				{
-					for (int i = 0; i < params.size(); i++)
-					{
-						ps.setObject(i + 1, params.get(i));
-					}
-				}
-				ps.executeUpdate();
-			}
-			conn.commit();
-			conn.setAutoCommit(true);
-		}
-		catch (SQLException | SecurityException e)
-		{
-			try
-			{
-				conn.rollback();
-			}
-			catch (SQLException e1)
-			{
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			e.printStackTrace();
-		}
-		finally
-		{
-			DBTool.closeAll(rs, ps, conn);
-		}
-	}
+    public void executeUpdateBatch(Map<String, List<Object>> queryMap) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = DBTool.getInstance().getConnection();
+            conn.setTransactionIsolation(8);
+            conn.setAutoCommit(false);
+            Set<String> querySet = queryMap.keySet();
+            Iterator<String> it = querySet.iterator();
+            while (it.hasNext()) {
+                String query = it.next();
+                ps = conn.prepareStatement(query);
+                List<Object> params = queryMap.get(query);
+                if (params.size() > 0) {
+                    for (int i = 0; i < params.size(); i++) {
+                        ps.setObject(i + 1, params.get(i));
+                    }
+                }
+                ps.executeUpdate();
+            }
+            conn.commit();
+            conn.setAutoCommit(true);
+        } catch (SQLException | SecurityException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+        } finally {
+            DBTool.closeAll(rs, ps, conn);
+        }
+    }
 
-	@Override
-	public int update(Object obj)
-	{
-		int row = 0;
-		try
-		{
-			// 获取到对象声明的所有属性字段
-			Field[] fields = obj.getClass().getDeclaredFields();
-			Field primaryField = null;
-			List<Object> params = new ArrayList<Object>();
-			StringBuffer sb = new StringBuffer();
-			sb.append("UPDATE ");
-			sb.append(obj.getClass().getSimpleName());
-			sb.append(" SET ");
-			for (int i = 0; i < fields.length; i++)
-			{
-				fields[i].setAccessible(true);
-				// 如果字段上标记了@Id注解，表示这是一个主键
-				boolean b = fields[i].isAnnotationPresent(Id.class);
-				if (!b)
-				{
-					if (i < fields.length - 1)
-					{
-						sb.append(fields[i].getName() + "=?,");
-					}
-					else
-					{
-						sb.append(fields[i].getName() + "=? ");
-					}
-					params.add(fields[i].get(obj));
-				}
-				else
-				{
-					primaryField = fields[i]; // 获取主键属性
-				}
-			}
-			sb.append(" WHERE " + primaryField.getName() + "=?");
-			params.add(primaryField.get(obj));
-			row = executeUpdate(sb.toString(), params);
-		}
-		catch (IllegalArgumentException | IllegalAccessException e)
-		{
-			e.printStackTrace();
-		}
-		return row;
-	}
+    @Override
+    public int update(Object obj) {
+        int row = 0;
+        try {
+            // 获取到对象声明的所有属性字段
+            Field[] fields = obj.getClass().getDeclaredFields();
+            Field primaryField = null;
+            List<Object> params = new ArrayList<Object>();
+            StringBuffer sb = new StringBuffer();
+            sb.append("UPDATE ");
+            sb.append(obj.getClass().getSimpleName());
+            sb.append(" SET ");
+            for (int i = 0; i < fields.length; i++) {
+                fields[i].setAccessible(true);
+                // 如果字段上标记了@Id注解，表示这是一个主键
+                boolean b = fields[i].isAnnotationPresent(Id.class);
+                if (!b) {
+                    if (i < fields.length - 1) {
+                        sb.append(fields[i].getName() + "=?,");
+                    } else {
+                        sb.append(fields[i].getName() + "=? ");
+                    }
+                    params.add(fields[i].get(obj));
+                } else {
+                    primaryField = fields[i]; // 获取主键属性
+                }
+            }
+            sb.append(" WHERE " + primaryField.getName() + "=?");
+            params.add(primaryField.get(obj));
+            row = executeUpdate(sb.toString(), params);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return row;
+    }
 
-	@Override
-	public int add(Object obj)
-	{
-		int row = 0;
-		try
-		{
-			// 获取到对象声明的所有属性字段
-			Field[] fields = obj.getClass().getDeclaredFields();
-			List<Object> params = new ArrayList<Object>();
-			StringBuffer sb = new StringBuffer();
-			sb.append("INSERT INTO ");
-			sb.append(obj.getClass().getSimpleName());
-			sb.append(" (");
-			for (int i = 0; i < fields.length; i++)
-			{
-				fields[i].setAccessible(true);
-				// 如果字段上标记了@Id注解，表示这是一个主键
-				boolean b = fields[i].isAnnotationPresent(Id.class);
-				if (!b)
-				{
-					if (i < fields.length - 1)
-					{
-						sb.append(fields[i].getName() + ",");
-					}
-					else
-					{
-						sb.append(fields[i].getName() + ") VALUES (");
-					}
-					params.add(fields[i].get(obj));
-				}
-			}
-			for (int i = 0; i < params.size() - 1; i++)
-			{
-				sb.append("?,");
-			}
-			sb.append("?)");
-			System.out.println(sb.toString());
-			row = executeUpdate(sb.toString(), params);
-		}
-		catch (IllegalArgumentException | IllegalAccessException e)
-		{
-			e.printStackTrace();
-		}
-		return row;
-	}
+    @Override
+    public int add(Object obj) {
+        int row = 0;
+        try {
+            // 获取到对象声明的所有属性字段
+            Field[] fields = obj.getClass().getDeclaredFields();
+            List<Object> params = new ArrayList<Object>();
+            StringBuffer sb = new StringBuffer();
+            sb.append("INSERT INTO ");
+            sb.append(obj.getClass().getSimpleName());
+            sb.append(" (");
+            for (int i = 0; i < fields.length; i++) {
+                fields[i].setAccessible(true);
+                // 如果字段上标记了@Id注解，表示这是一个主键
+                boolean b = fields[i].isAnnotationPresent(Id.class);
+                if (!b) {
+                    if (i < fields.length - 1) {
+                        sb.append(fields[i].getName() + ",");
+                    } else {
+                        sb.append(fields[i].getName() + ") VALUES (");
+                    }
+                    params.add(fields[i].get(obj));
+                }
+            }
+            for (int i = 0; i < params.size() - 1; i++) {
+                sb.append("?,");
+            }
+            sb.append("?)");
+            System.out.println(sb.toString());
+            row = executeUpdate(sb.toString(), params);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return row;
+    }
 
-	@Override
-	public int delete(Object obj)
-	{
-		int row = 0;
-		try
-		{
-			// 获取到对象声明的所有属性字段
-			Field[] fields = obj.getClass().getDeclaredFields();
-			Field primaryField = null;
-			List<Object> params = new ArrayList<Object>();
-			StringBuffer sb = new StringBuffer();
-			sb.append("DELETE FROM ");
-			sb.append(obj.getClass().getSimpleName());
-			for (int i = 0; i < fields.length; i++)
-			{
-				fields[i].setAccessible(true);
-				// 如果字段上标记了@Id注解，表示这是一个主键
-				boolean b = fields[i].isAnnotationPresent(Id.class);
-				if (b)
-				{
-					primaryField = fields[i]; // 获取主键属性
-				}
-			}
-			sb.append(" WHERE " + primaryField.getName() + "=?");
-			System.out.println(sb.toString());
-			params.add(primaryField.get(obj));
-			row = executeUpdate(sb.toString(), params);
-		}
-		catch (IllegalArgumentException | IllegalAccessException e)
-		{
-			e.printStackTrace();
-		}
-		return row;
-	}
+    @Override
+    public int delete(Object obj) {
+        int row = 0;
+        try {
+            // 获取到对象声明的所有属性字段
+            Field[] fields = obj.getClass().getDeclaredFields();
+            Field primaryField = null;
+            List<Object> params = new ArrayList<Object>();
+            StringBuffer sb = new StringBuffer();
+            sb.append("DELETE FROM ");
+            sb.append(obj.getClass().getSimpleName());
+            for (int i = 0; i < fields.length; i++) {
+                fields[i].setAccessible(true);
+                // 如果字段上标记了@Id注解，表示这是一个主键
+                boolean b = fields[i].isAnnotationPresent(Id.class);
+                if (b) {
+                    primaryField = fields[i]; // 获取主键属性
+                }
+            }
+            sb.append(" WHERE " + primaryField.getName() + "=?");
+            System.out.println(sb.toString());
+            params.add(primaryField.get(obj));
+            row = executeUpdate(sb.toString(), params);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return row;
+    }
 }
--- src/com/company/entity/CarInformation.java
+++ src/com/company/entity/CarInformation.java
@@ -1,6 +1,28 @@
 package com.company.entity;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
 public class CarInformation {
+
+    public String Car_Id;
+    public Timestamp Buy_Time;
+    public String Car_Type;
+    public String Car_Color;
+    public String Chuchang_Id;
+    public String Motor_Id;
+    public String Dipan_Id;
+    public String Chair_Num;
+
+    public BigDecimal Buy_Price;
+    public BigDecimal Buy_tax;
+    public BigDecimal Shangpai_fee;
+    public BigDecimal Car_Zhuangshi;
+    public String Chair_Num;
+    public String Chair_Num;
+    public BigDecimal Start_Gongli;
+    public BigDecimal Current_Gongli;
+
     /*
     CREATE TABLE `tb_car` (
   `Car_Id` varchar(10) NOT NULL COMMENT '车牌号',
@@ -11,6 +33,7 @@ public class CarInformation {
   `Motor_Id` varchar(10) DEFAULT NULL COMMENT '发动机号',
   `Dipan_Id` varchar(10) DEFAULT NULL COMMENT '底盘编号',
   `Chair_Num` varchar(5) DEFAULT NULL COMMENT '座位数',
+
   `Buy_Price` decimal(8,2) DEFAULT NULL COMMENT '购买价格',
   `Buy_tax` decimal(8,2) DEFAULT NULL COMMENT '购置税',
   `Shangpai_fee` decimal(8,2) DEFAULT NULL COMMENT '上牌费',
@@ -19,6 +42,7 @@ public class CarInformation {
   `Tel` varchar(11) DEFAULT NULL COMMENT '联系电话',
   `Start_Gongli` decimal(8,1) DEFAULT NULL COMMENT '起始公里数',
   `Current_Gongli` decimal(8,1) DEFAULT NULL COMMENT '当前公里数',
+
   `YLF_Starttime` timestamp NULL DEFAULT NULL COMMENT '养路费购买时间',
   `YLF_Endtime` timestamp NULL DEFAULT NULL COMMENT '养路费截止时间',
   `NS_Starttime` timestamp NULL DEFAULT NULL COMMENT '年审时间',
@@ -33,6 +57,7 @@ public class CarInformation {
   `YYZ_Endtime` timestamp NULL DEFAULT NULL COMMENT '营运证截止时间',
   `YGF_Starttime` timestamp NULL DEFAULT NULL COMMENT '运营证购买时间',
   `YGF_Endtime` timestamp NULL DEFAULT NULL COMMENT '运营证截止时间',
+
   `Erbaolicheng` decimal(8,0) DEFAULT NULL COMMENT '二保里程',
   `NextErbao` decimal(8,0) DEFAULT NULL COMMENT '下次二保',
      */

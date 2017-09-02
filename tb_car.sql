f19047ca9142f14e87b11d4e5b71f26681595332
Ross.zhang <yuefeiduzi@foxmail.com>
2017-09-02 16:51:11 +0800

    汽车档案编辑frame @2017年8月30日13:30:58 汽车档案编辑frame 大体 @2017年8月30日20:38:48 窗体完成 @2017年8月30日21:22:24 已知BUG：JSpinner换皮肤时有失去光标的空指针异常 @2017年8月30日21:50:00 JSpinner方法设定不能输入非法值，不能越界 @2017年8月31日9:51:53 normal update @2017年8月31日14:09:18 换肤frame @2017年8月31日16:48:23 换肤完成 @2017年8月31日21:03:06 DAO导入完毕 @2017年9月1日11:34:40 写实例化 @2017年9月1日16:49:22 sql语句没错但是程序执行时有错（疑是编码问题） @2017年9月1日21:50:10 问题解决没有改conf/dbms.properties下的用户参数 @2017年9月2日9:53:59 添加细枝末节 @2017年9月2日11:48:47 正在实现存盘excel @2017年9月2日16:48:50

---
+++ tb_car.sql
@@ -0,0 +1,43 @@
+INSERT INTO `tb_car` (`Car_Id`, `Buy_Time`, `Car_Type`, `Car_Color`, `Chuchang_Id`, `Motor_Id`, `Dipan_Id`, `Chair_Num`, `Buy_Price`, `Buy_tax`, `Shangpai_fee`, `Zhuangshi`, `Chezhu_Name`, `Tel`, `Start_Gongli`, `Current_Gongli`, `YLF_Starttime`, `YLF_Endtime`, `NS_Starttime`, `NS_Endtime`, `BX_Starttime`, `BX_Endtime`, `CCS_Starttime`, `CCS_Endtime`, `LQP_Starttime`, `LQP_Endtime`, `YYZ_Starttime`, `YYZ_Endtime`, `YGF_Starttime`, `YGF_Endtime`, `Erbaolicheng`, `NextErbao`) VALUES ('1123', '2017-9-2 09:48:41', '小轿车', '黑', '11', '11', '11', '6', 11.00, 11.00, 11.00, 11, '11', '11', 11.0, 11.0, '2017-9-2 09:48:41', '2018-9-2 09:48:41', '2017-9-2 09:48:41', '2018-9-2 09:48:41', '2017-9-2 09:48:41', '2018-9-2 09:48:41', '2017-9-2 09:48:41', '2018-9-2 09:48:41', '2017-9-2 09:48:41', '2018-9-2 09:48:41', '2017-9-2 09:48:41', '2018-9-2 09:48:41', '2017-9-2 09:48:41', '2018-9-2 09:48:41', 11, 11);
+INSERT INTO `tb_car` (`Car_Id`, `Buy_Time`, `Car_Type`, `Car_Color`, `Chuchang_Id`, `Motor_Id`, `Dipan_Id`, `Chair_Num`, `Buy_Price`, `Buy_tax`, `Shangpai_fee`, `Zhuangshi`, `Chezhu_Name`, `Tel`, `Start_Gongli`, `Current_Gongli`, `YLF_Starttime`, `YLF_Endtime`, `NS_Starttime`, `NS_Endtime`, `BX_Starttime`, `BX_Endtime`, `CCS_Starttime`, `CCS_Endtime`, `LQP_Starttime`, `LQP_Endtime`, `YYZ_Starttime`, `YYZ_Endtime`, `YGF_Starttime`, `YGF_Endtime`, `Erbaolicheng`, `NextErbao`) VALUES ('1124', '2017-9-2 11:13:12', '小轿车', '黑', '11', '11', '11', '4', 11.00, 11.00, 11.00, 11, '11', '11', 11.0, 11.0, '2017-9-2 11:13:12', '2018-9-2 11:13:12', '2017-9-2 11:13:12', '2018-9-2 11:13:12', '2017-9-2 11:13:12', '2018-9-2 11:13:12', '2017-9-2 11:13:12', '2018-9-2 11:13:12', '2017-9-2 11:13:12', '2018-9-2 11:13:12', '2017-9-2 11:13:12', '2018-9-2 11:13:12', '2017-9-2 11:13:12', '2018-9-2 11:13:12', 11, 111);
+INSERT INTO `tb_car` (`Car_Id`, `Buy_Time`, `Car_Type`, `Car_Color`, `Chuchang_Id`, `Motor_Id`, `Dipan_Id`, `Chair_Num`, `Buy_Price`, `Buy_tax`, `Shangpai_fee`, `Zhuangshi`, `Chezhu_Name`, `Tel`, `Start_Gongli`, `Current_Gongli`, `YLF_Starttime`, `YLF_Endtime`, `NS_Starttime`, `NS_Endtime`, `BX_Starttime`, `BX_Endtime`, `CCS_Starttime`, `CCS_Endtime`, `LQP_Starttime`, `LQP_Endtime`, `YYZ_Starttime`, `YYZ_Endtime`, `YGF_Starttime`, `YGF_Endtime`, `Erbaolicheng`, `NextErbao`) VALUES ('1223', '2017-9-2 11:37:23', '小轿车', '黑', '21', '21', '21', '4', 312.00, 21.00, 32.00, 21, '12', '21', 12.0, 21.0, '2017-9-2 11:37:23', '2018-9-2 11:37:23', '2017-9-2 11:37:23', '2018-9-2 11:37:23', '2017-9-2 11:37:23', '2018-9-2 11:37:23', '2017-9-2 11:37:23', '2018-9-2 11:37:23', '2017-9-2 11:37:23', '2018-9-2 11:37:23', '2017-9-2 11:37:23', '2018-9-2 11:37:23', '2017-9-2 11:37:23', '2018-9-2 11:37:23', 21, 21);
+INSERT INTO `tb_car` (`Car_Id`, `Buy_Time`, `Car_Type`, `Car_Color`, `Chuchang_Id`, `Motor_Id`, `Dipan_Id`, `Chair_Num`, `Buy_Price`, `Buy_tax`, `Shangpai_fee`, `Zhuangshi`, `Chezhu_Name`, `Tel`, `Start_Gongli`, `Current_Gongli`, `YLF_Starttime`, `YLF_Endtime`, `NS_Starttime`, `NS_Endtime`, `BX_Starttime`, `BX_Endtime`, `CCS_Starttime`, `CCS_Endtime`, `LQP_Starttime`, `LQP_Endtime`, `YYZ_Starttime`, `YYZ_Endtime`, `YGF_Starttime`, `YGF_Endtime`, `Erbaolicheng`, `NextErbao`) VALUES ('1234', '2017-9-2 10:15:21', '小轿车', '黑', '22', '22', '22', '4', 22.00, 22.00, 22.00, 22, '22', '22', 22.0, 22.0, '2017-9-2 10:15:21', '2018-9-2 10:15:21', '2017-9-2 10:15:21', '2018-9-2 10:15:21', '2017-9-2 10:15:21', '2018-9-2 10:15:21', '2017-9-2 10:15:21', '2018-9-2 10:15:21', '2017-9-2 10:15:21', '2018-9-2 10:15:21', '2017-9-2 10:15:21', '2018-9-2 10:15:21', '2017-9-2 10:15:21', '2018-9-2 10:15:21', 22, 22);
+INSERT INTO `tb_car` (`Car_Id`, `Buy_Time`, `Car_Type`, `Car_Color`, `Chuchang_Id`, `Motor_Id`, `Dipan_Id`, `Chair_Num`, `Buy_Price`, `Buy_tax`, `Shangpai_fee`, `Zhuangshi`, `Chezhu_Name`, `Tel`, `Start_Gongli`, `Current_Gongli`, `YLF_Starttime`, `YLF_Endtime`, `NS_Starttime`, `NS_Endtime`, `BX_Starttime`, `BX_Endtime`, `CCS_Starttime`, `CCS_Endtime`, `LQP_Starttime`, `LQP_Endtime`, `YYZ_Starttime`, `YYZ_Endtime`, `YGF_Starttime`, `YGF_Endtime`, `Erbaolicheng`, `NextErbao`) VALUES ('22', '2017-9-2 09:45:40', '小轿车', '黑', '11', '1', '11', '4', 11.00, 1.00, 11.00, 1, '11', '11', 11.0, 11.0, '2017-9-2 09:45:40', '2018-9-2 09:45:40', '2017-9-2 09:45:40', '2018-9-2 09:45:40', '2017-9-2 09:45:40', '2018-9-2 09:45:40', '2017-9-2 09:45:40', '2018-9-2 09:45:40', '2017-9-2 09:45:40', '2018-9-2 09:45:40', '2017-9-2 09:45:40', '2018-9-2 09:45:40', '2017-9-2 09:45:40', '2018-9-2 09:45:40', 11, 11);
+
+
+CREATE TABLE `tb_car` (
+  `Car_Id` varchar(10) NOT NULL COMMENT '车牌号',
+  `Buy_Time` timestamp NULL DEFAULT NULL COMMENT '购买时间',
+  `Car_Type` varchar(50) DEFAULT NULL COMMENT '车型',
+  `Car_Color` varchar(20) DEFAULT NULL COMMENT '颜色',
+  `Chuchang_Id` varchar(10) DEFAULT NULL COMMENT '出厂编号',
+  `Motor_Id` varchar(10) DEFAULT NULL COMMENT '发动机号',
+  `Dipan_Id` varchar(10) DEFAULT NULL COMMENT '底盘编号',
+  `Chair_Num` varchar(5) DEFAULT NULL COMMENT '座位数',
+  `Buy_Price` decimal(8,2) DEFAULT NULL COMMENT '购买价格',
+  `Buy_tax` decimal(8,2) DEFAULT NULL COMMENT '购置税',
+  `Shangpai_fee` decimal(8,2) DEFAULT NULL COMMENT '上牌费',
+  `Zhuangshi` decimal(8,0) DEFAULT NULL COMMENT '汽车装饰',
+  `Chezhu_Name` varchar(10) DEFAULT NULL COMMENT '车主姓名',
+  `Tel` varchar(11) DEFAULT NULL COMMENT '联系电话',
+  `Start_Gongli` decimal(8,1) DEFAULT NULL COMMENT '起始公里数',
+  `Current_Gongli` decimal(8,1) DEFAULT NULL COMMENT '当前公里数',
+  `YLF_Starttime` timestamp NULL DEFAULT NULL COMMENT '养路费购买时间',
+  `YLF_Endtime` timestamp NULL DEFAULT NULL COMMENT '养路费截止时间',
+  `NS_Starttime` timestamp NULL DEFAULT NULL COMMENT '年审时间',
+  `NS_Endtime` timestamp NULL DEFAULT NULL COMMENT '年审截止时间',
+  `BX_Starttime` timestamp NULL DEFAULT NULL COMMENT '保险购买时间',
+  `BX_Endtime` timestamp NULL DEFAULT NULL COMMENT '保险截止时间',
+  `CCS_Starttime` timestamp NULL DEFAULT NULL COMMENT '车船税购买时间',
+  `CCS_Endtime` timestamp NULL DEFAULT NULL COMMENT '车船税截止时间',
+  `LQP_Starttime` timestamp NULL DEFAULT NULL COMMENT '路桥票购买时间',
+  `LQP_Endtime` timestamp NULL DEFAULT NULL COMMENT '路桥票截止时间',
+  `YYZ_Starttime` timestamp NULL DEFAULT NULL COMMENT '营运证办理时间',
+  `YYZ_Endtime` timestamp NULL DEFAULT NULL COMMENT '营运证截止时间',
+  `YGF_Starttime` timestamp NULL DEFAULT NULL COMMENT '运营证购买时间',
+  `YGF_Endtime` timestamp NULL DEFAULT NULL COMMENT '运营证截止时间',
+  `Erbaolicheng` decimal(8,0) DEFAULT NULL COMMENT '二保里程',
+  `NextErbao` decimal(8,0) DEFAULT NULL COMMENT '下次二保',
+  PRIMARY KEY (`Car_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
